db48b73357d58b914ecde273bfcbbee2fab876f3
Sriharivas5 <sriharivas5@gmail.com>
2024-02-17 12:37:39 +0530

    deplo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/https://Sriharivas5.github.io/multiverse" element={<Home />} />
+        <Route path="/multiverse" element={<Home />} />
         <Route path="/blackhole" element={<BlackHoles />} />
         <Route path="/darkMatter" element={<DarkMatter />} />
       </Routes>
